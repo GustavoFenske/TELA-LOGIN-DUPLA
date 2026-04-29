6ada7401ac7bf29e5f0b07bf744c487bea56ea52
GustavoFenske <gustavo_fenske@estudante.sesisenai.org.br>
2026-04-29 11:19:18 -0300

    ref - retirei o cadastro e modifiquei as validações

--- script.js
+++ script.js
@@ -11,32 +11,19 @@ document.getElementById("toggle").onclick = () => {
 document.getElementById("form-login").onsubmit = (e) => {
     e.preventDefault();
 
-    let email = document.getElementById("email").value;
+    let usuario = document.getElementById("usuario").value;
     let senha = document.getElementById("senha").value;
     let mensagem = document.getElementById("mensagem").value;
 
     mensagem.innerHTML = " ";
 
-    if (!email.includes("@") || !email.includes(".")) {
-        mensagem.innerHTML = "<div class='erro'><p> Email Inválido!</p></div>";
+    if (usuario.length < 0 ) {
+        mensagem.innerHTML = "<div class='erro'><p> digite um usuário!</p></div>";
         return;
     }
 
-    if (senha.length < 4) {
+    if (senha.length <= 0) {
         mensagem.innerHTML = "<div class='erro'><p> Senha muito curta!</p></div>";
         return;
-    }
-
-    if (cadastro) {
-        localStorage.setItem(email, senha);
-        mensagem.innerHTML = "<div class='sucesso'><p> Cadastrado com sucesso!</p></div>";
-    } else {
-        let salva = localStorage.getItem(email);
-        if (salva === senha) {
-            mensagem.innerHTML = "<div class='sucesso'><p>Login com sucesso!</p></div>";
-        } else {
-        }
-        mensagem.innerHTML = "<div class='erro'><p>Dados Incorretos!</p></div>";
-        document.getElementById("form-login").reset();
     }
 }
